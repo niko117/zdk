0563bc3f3a51b766e222737fed77c1843c99a8e7
nik <support@zeboost.com>
2018-09-07 22:37:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+NXBoost (NXB) is a universal 3rd generation cryptocurrency that combines the best features of different coins
+in order to create an excellent new digital payment asset.
+
+Milli-Second-Transactions with HyperSend.
+Network stability with masternodes, each is secured with a collateral of 10.000 NXBoost. 100% Governance.
+
+For further information visit us at [nxboost.org](https://nxboost.org/) or visit our ANN thread at [BitcoinTalk](https://bitcointalk.org/index.php?topic=2343884.0)
+
+# Coin Specs
+
+• Coin Name: NXBoost  
+• Ticker: NXB  
+• PoW Algorithm: Quark  
+• Premine: (#1 Block) 100,001 NXBoost (0,17% of PoW)*  
+• PoW Blocks: 2 - 475200  
+• PoS Blocks: Starting from 475201  
+• Block Time: 60 Seconds  
+• PoW Max Coin Output/Supply: 57,879,300  
+• PoW Ending: ~ ca. 330 Days (Estimated: October 2018)  
+• Masternode Requirements: 10,000 NXB  
+• Maturity: 111 Confirmations  
+• Prefix: NXBoost adresses start with the capital letter "A"   
+
+*the premine is going to be burned at the 1st November 2018
+
+# PoW Reward Distribution
+
+<table>
+  <tr><th>Block Height</th><th>Reward Amount</th><th>Masternodes</th><th>Miners</th><th>DevFee</th></tr>
+  <tr><td>Block 2 - 86400</td><td>80 NXB</td><td>   Masternodes 40 NXB</td><td>Miners 40 NXB</td><td>0 NXB</td></tr>
+<tr><td>Block 86401 - 151200</td><td>70 NXB</td><td>  Masternodes 38.5 NXB</td><td>Miners 31.5 NXB</td><td>0 NXB</td></tr>
+<tr><td>Block 151201 - 225000</td><td>60 NXB</td><td>  Masternodes 36 NXB</td><td>Miners 24 NXB</td><td>0 NXB</td></tr>
+<tr><td>Block 225001 - 302400</td><td>50 NXB</td><td>  Masternodes 35 NXB</td><td>Miners 15 NXB</td><td>6,25 NXB</td></tr>
+<tr><td>Block 302401 - 345600</td><td>40 NXB</td><td>   Masternodes 28 NXB</td><td>Miners 12 NXB</td><td>5 NXB</td></tr>
+<tr><td>Block 345601 - 388800</td><td>30 NXB</td><td>      Masternodes 21 NXB</td><td>Miners 9 NXB</td><td>3,75 NXB</td></tr>
+<tr><td>Block 388801 - 475200</td><td>10 NXB</td><td>      Masternodes 7 NXB</td><td>Miners 3 NXB</td><td>2,5 NXB</td></tr>
+</table>
+
+Masternode and staker rewards with PoS:
+Masternodes and stakers will receive split reward allocation when PoS sets in.
+This means the more masternodes are in the system, the more staking gets attractive and vice versa.
+
+# PoS Block Rewards
+
+_Proof of Stake will automatically be enabled at block 475201._
+<table>
+<tr><th>Block Height</th><th>Reward Amount</th>                  
+<tr><td>Block 475201 - 518400</td><td>5 NXB</td></tr>
+<tr><td>Block 518401 - 561600</td><td>3 NXB</td></tr>
+<tr><td>Block 561601 - 604800</td><td>2 NXB</td></tr>
+<tr><td>Block 604801 - infinite</td><td>5 NXB</td></tr>
+</table>
+
+
+# Further information
+
+_For more information check out our whitepaper at [nxboost.org/whitepaper.pdf](https://nxboost.org/whitepaper.pdf)_
+
+
+Alternatively we have added a non .pdf Version into our Github. You can find it in the NXBoost Github Wiki.
+
+[https://github.com/NXBoostCRYPTO/NXBoost/wiki](https://github.com/NXBoostCRYPTO/NXBoost/wiki)
---
+++ src/chainparams.cpp
@@ -0,0 +1,460 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2014-2015 The Dash developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2015-2017 The NXBoost developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chainparams.h"
+
+#include "random.h"
+#include "util.h"
+#include "utilstrencodings.h"
+
+#include <assert.h>
+
+#include <boost/assign/list_of.hpp>
+
+
+using namespace std;
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+#include "bignum.h"
+/**
+ * Main network
+ */
+
+//! Convert the pnSeeds6 array into usable address objects.
+static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data, unsigned int count)
+{
+    // It'll only connect to one or two seed nodes because once it connects,
+    // it'll get a pile of addresses with newer timestamps.
+    // Seed nodes are given a random 'last seen time' of between one and two
+    // weeks ago.
+    const int64_t nOneWeek = 7 * 24 * 60 * 60;
+    for (unsigned int i = 0; i < count; i++) {
+        struct in6_addr ip;
+        memcpy(&ip, data[i].addr, sizeof(ip));
+        CAddress addr(CService(ip, data[i].port));
+        addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+//   What makes a good checkpoint block?
+// + Is surrounded by blocks with reasonable timestamps
+//   (no blocks before with a timestamp after, none after with
+//    timestamp before)
+// + Contains no strange transactions
+
+static Checkpoints::MapCheckpoints mapCheckpoints =
+    boost::assign::map_list_of
+	(0, uint256("0x00000a34ab063076ba734c158aabdc323fb83896fe5a18a9d26c55636d29d1fa"));
+
+static const Checkpoints::CCheckpointData data = {
+    &mapCheckpoints,
+    1536328694, // * UNIX timestamp of last checkpoint block
+    0,    // * total number of transactions between genesis and last checkpoint
+                //   (the tx=... number in the SetBestChain debug.log lines)
+    2000        // * estimated number of transactions per day after checkpoint
+};
+
+static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
+    boost::assign::map_list_of(0, uint256("0"));
+static const Checkpoints::CCheckpointData dataTestnet = {
+    &mapCheckpointsTestnet,
+    0,
+    0,
+    250};
+
+static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
+    boost::assign::map_list_of(0, uint256("0"));
+static const Checkpoints::CCheckpointData dataRegtest = {
+    &mapCheckpointsRegtest,
+    0,
+    0,
+    100};
+
+class CMainParams : public CChainParams
+{
+public:
+    CMainParams()
+    {
+        networkID = CBaseChainParams::MAIN;
+        strNetworkID = "main";
+        /**
+         * The message start string is designed to be unlikely to occur in normal data.
+         * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+         * a large 4-byte int at any alignment.
+         */
+        pchMessageStart[0] = 0x94;
+        pchMessageStart[1] = 0x04;
+        pchMessageStart[2] = 0x15;
+        pchMessageStart[3] = 0x14;
+        vAlertPubKey = ParseHex("0497dfcea626dca270cfe2eebf1160733b07352af966926e30058ca53dae05bbc71d64dbaa0c66e373211e324a23b361fc33e1e2dece1fb90afcefc707643a28f5");
+        nDefaultPort = 49108;
+        bnProofOfWorkLimit = ~uint256(0) >> 20; // NXBoost starting difficulty is 1 / 2^12  //20
+        nSubsidyHalvingInterval = 210000;
+        nMaxReorganizationDepth = 100;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // NXBoost: 1 day
+        nTargetSpacing = 1 * 60;  // NXBoost: 1 minute
+        nLastPOWBlock = 475200;
+        nMaturity = 110;
+        nMasternodeCountDrift = 20;
+		nMasternodeColleteralLimxDev = 999999; //Params().MasternodeColleteralLimxDev()
+        nModifierUpdateBlock = 1; // we use the version 2 for dmd
+        nMaxMoneyOut = 100000000 * COIN;
+		strDeveloperFeePayee = "AG6MCBRozzcQMMi2SXdi4TJhhLeycQERyk";
+
+        const char* pszTimestamp = "NXBoost Reload august 2018";
+        CMutableTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 0 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040fbf75ea45bf7eb65f9075e8342b7520d077b66f964e9b1963c96cefd9a2c52e93d0def918e0cd3c91f42821f429b2be4505552dede31c96f95f0d843da0beae") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime = 1536328694;
+        genesis.nBits = 0x1e0ffff0;
+        genesis.nNonce = 457560;		
+/*if (genesis.GetHash() != hashGenesisBlock) {
+            printf("Searching for genesis block...\n");
+            uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+
+            uint256 thash;
+            while (true) {
+                thash = genesis.GetHash();
+                if (thash <= hashTarget)
+                    break;
+                if ((genesis.nNonce & 0xFFF) == 0) {
+                    printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                }
+                ++genesis.nNonce;
+                if (genesis.nNonce == 0) {
+                    printf("NONCE WRAPPED, incrementing time\n");
+                    ++genesis.nTime;
+                }
+            }
+            printf("genesis.nTime = %u \n", genesis.nTime);
+            printf("genesis.nNonce = %u \n", genesis.nNonce);
+            printf("genesis.nVersion = %u \n", genesis.nVersion);
+            printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+            printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            exit(0);
+        }*/
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x00000a34ab063076ba734c158aabdc323fb83896fe5a18a9d26c55636d29d1fa"));
+        assert(genesis.hashMerkleRoot == uint256("0x32268b701d69e7aa6be82e213a3d6b309a2090ef4e0b6893d472f58a748de5c8"));
+
+        vSeeds.push_back(CDNSSeedData("89.40.4.42", "89.40.4.42"));
+        vSeeds.push_back(CDNSSeedData("213.80.163.129", "213.80.163.129"));
+        vSeeds.push_back(CDNSSeedData("185.159.82.234", "185.159.82.234"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 52);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 52);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 193);
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
+        // 	BIP44 coin type is from https://github.com/satoshilabs/slips/blob/master/slip-0044.md 109 	0x8000006d
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x6d).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = true;
+        fAllowMinDifficultyBlocks = false;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+        fSkipProofOfWorkCheck = false;
+        fTestnetToBeDeprecatedFieldRPC = false;
+        fHeadersFirstSyncingActive = false;
+
+
+        nPoolMaxTransactions = 3;
+        strSporkKey = "049e53e687fdafd78fd42d730fad0e7ea1819396176a2cb85d7a76fa4559cdbd2c2f05330a6f5cbadb44a6c1d324f167e679e9f3e95d9d5649761a3e7f59bf4500";
+        strDarksendPoolDummyAddress = "AcmpqXViWUXNroqVNYRdKjKrFM6PNa1oTM";
+        nStartMasternodePayments = 1536328694; //11/10/2017 @ 12:00am (UTC)
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return data;
+    }
+};
+static CMainParams mainParams;
+
+/**
+ * Testnet (v3)
+ */
+class CTestNetParams : public CMainParams
+{
+public:
+    CTestNetParams()
+    {
+        networkID = CBaseChainParams::TESTNET;
+        strNetworkID = "test";
+        pchMessageStart[0] = 0x64;
+        pchMessageStart[1] = 0x44;
+        pchMessageStart[2] = 0x65;
+        pchMessageStart[3] = 0x54;
+        vAlertPubKey = ParseHex("04e1d34432d3a5803312f33cf20686b1801835ea8efb18ad77d50b5ec1ce5ca73de65c36c082ca5c51880f8ffb5a296d8dc780f1dc02f22eba4dde1816a6a54b75");
+        nDefaultPort = 55600;
+        nEnforceBlockUpgradeMajority = 51;
+        nRejectBlockOutdatedMajority = 75;
+        nToCheckBlockUpgradeMajority = 100;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // NXBoost: 1 day
+        nTargetSpacing = 1 * 60;  // NXBoost: 1 minute
+        nLastPOWBlock = 200;
+        nMaturity = 15;
+        nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
+        nMaxMoneyOut = 100000000 * COIN;
+
+        //! Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime = 1509321602;
+        genesis.nNonce = 1486441;
+/*if (genesis.GetHash() != hashGenesisBlock) {
+            printf("Searching for genesis block...\n");
+            uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+
+            uint256 thash;
+            while (true) {
+                thash = genesis.GetHash();
+                if (thash <= hashTarget)
+                    break;
+                if ((genesis.nNonce & 0xFFF) == 0) {
+                    printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                }
+                ++genesis.nNonce;
+                if (genesis.nNonce == 0) {
+                    printf("NONCE WRAPPED, incrementing time\n");
+                    ++genesis.nTime;
+                }
+            }
+            printf("genesis.nTime = %u \n", genesis.nTime);
+            printf("genesis.nNonce = %u \n", genesis.nNonce);
+            printf("genesis.nVersion = %u \n", genesis.nVersion);
+            printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+            printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            exit(0);
+        }
+*/
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x0000071f2159db5c6c775067b2b122a61b4d1965236de30d76f21a91b23f317e"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+		
+		vSeeds.push_back(CDNSSeedData("127.0.0.1", "127.0.0.1"));
+    
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 83);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 18);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 193);
+        // Testnet nxboost BIP32 pubkeys start with 'DRKV'
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x3a)(0x80)(0x61)(0xa0).convert_to_container<std::vector<unsigned char> >();
+        // Testnet nxboost BIP32 prvkeys start with 'DRKP'
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x3a)(0x80)(0x58)(0x37).convert_to_container<std::vector<unsigned char> >();
+        // Testnet nxboost BIP44 coin type is '1' (All coin's testnet default)
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x01).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = true;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = false;
+        fTestnetToBeDeprecatedFieldRPC = true;
+
+
+
+        nPoolMaxTransactions = 2;
+        strSporkKey = "049e53e687fdafd78fd42d730fad0e7ea1819396176a2cb85d7a76fa4559cdbd2c2f05330a6f5cbadb44a6c1d324f167e679e9f3e95d9d5649761a3e7f59bf4500";
+        strDarksendPoolDummyAddress = "";
+        nStartMasternodePayments = 1510272000;
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataTestnet;
+    }
+};
+static CTestNetParams testNetParams;
+
+/**
+ * Regression test
+ */
+class CRegTestParams : public CTestNetParams
+{
+public:
+    CRegTestParams()
+    {
+        networkID = CBaseChainParams::REGTEST;
+        strNetworkID = "regtest";
+        strNetworkID = "regtest";
+        pchMessageStart[0] = 0x14;
+        pchMessageStart[1] = 0x54;
+        pchMessageStart[2] = 0x95;
+        pchMessageStart[3] = 0x64;
+        nSubsidyHalvingInterval = 150;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 1;
+        nTargetTimespan = 24 * 60 * 60; // NXBoost: 1 day
+        nTargetSpacing = 1 * 60;        // NXBoost: 1 minutes
+        bnProofOfWorkLimit = ~uint256(0) >> 1;
+        genesis.nTime = 1509321603;
+        genesis.nBits = 0x1e0ffff0;
+        genesis.nNonce = 277787;
+		/*
+if (genesis.GetHash() != hashGenesisBlock) {
+            printf("Searching for genesis block...\n");
+            uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+
+            uint256 thash;
+            while (true) {
+                thash = genesis.GetHash();
+                if (thash <= hashTarget)
+                    break;
+                if ((genesis.nNonce & 0xFFF) == 0) {
+                    printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                }
+                ++genesis.nNonce;
+                if (genesis.nNonce == 0) {
+                    printf("NONCE WRAPPED, incrementing time\n");
+                    ++genesis.nTime;
+                }
+            }
+            printf("genesis.nTime = %u \n", genesis.nTime);
+            printf("genesis.nNonce = %u \n", genesis.nNonce);
+            printf("genesis.nVersion = %u \n", genesis.nVersion);
+            printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+            printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            exit(0);
+        }*/
+
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 55700;
+        assert(hashGenesisBlock == uint256("0x00000bed4959f3ef5d2fff420ae551216c7108e5faddde8aa10fb039be46d0a0"));
+
+        vFixedSeeds.clear(); //! Testnet mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Testnet mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = true;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = true;
+        fTestnetToBeDeprecatedFieldRPC = false;
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataRegtest;
+    }
+};
+static CRegTestParams regTestParams;
+
+/**
+ * Unit test
+ */
+class CUnitTestParams : public CMainParams, public CModifiableParams
+{
+public:
+    CUnitTestParams()
+    {
+        networkID = CBaseChainParams::UNITTEST;
+        strNetworkID = "unittest";
+        nDefaultPort = 51478;
+        vFixedSeeds.clear(); //! Unit test mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Unit test mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fDefaultConsistencyChecks = true;
+        fAllowMinDifficultyBlocks = false;
+        fMineBlocksOnDemand = true;
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        // UnitTest share the same checkpoints as MAIN
+        return data;
+    }
+
+    //! Published setters to allow changing values in unit test cases
+    virtual void setSubsidyHalvingInterval(int anSubsidyHalvingInterval) { nSubsidyHalvingInterval = anSubsidyHalvingInterval; }
+    virtual void setEnforceBlockUpgradeMajority(int anEnforceBlockUpgradeMajority) { nEnforceBlockUpgradeMajority = anEnforceBlockUpgradeMajority; }
+    virtual void setRejectBlockOutdatedMajority(int anRejectBlockOutdatedMajority) { nRejectBlockOutdatedMajority = anRejectBlockOutdatedMajority; }
+    virtual void setToCheckBlockUpgradeMajority(int anToCheckBlockUpgradeMajority) { nToCheckBlockUpgradeMajority = anToCheckBlockUpgradeMajority; }
+    virtual void setDefaultConsistencyChecks(bool afDefaultConsistencyChecks) { fDefaultConsistencyChecks = afDefaultConsistencyChecks; }
+    virtual void setAllowMinDifficultyBlocks(bool afAllowMinDifficultyBlocks) { fAllowMinDifficultyBlocks = afAllowMinDifficultyBlocks; }
+    virtual void setSkipProofOfWorkCheck(bool afSkipProofOfWorkCheck) { fSkipProofOfWorkCheck = afSkipProofOfWorkCheck; }
+};
+static CUnitTestParams unitTestParams;
+
+
+static CChainParams* pCurrentParams = 0;
+
+CModifiableParams* ModifiableParams()
+{
+    assert(pCurrentParams);
+    assert(pCurrentParams == &unitTestParams);
+    return (CModifiableParams*)&unitTestParams;
+}
+
+const CChainParams& Params()
+{
+    assert(pCurrentParams);
+    return *pCurrentParams;
+}
+
+CChainParams& Params(CBaseChainParams::Network network)
+{
+    switch (network) {
+    case CBaseChainParams::MAIN:
+        return mainParams;
+    case CBaseChainParams::TESTNET:
+        return testNetParams;
+    case CBaseChainParams::REGTEST:
+        return regTestParams;
+    case CBaseChainParams::UNITTEST:
+        return unitTestParams;
+    default:
+        assert(false && "Unimplemented network");
+        return mainParams;
+    }
+}
+
+void SelectParams(CBaseChainParams::Network network)
+{
+    SelectBaseParams(network);
+    pCurrentParams = &Params(network);
+}
+
+bool SelectParamsFromCommandLine()
+{
+    CBaseChainParams::Network network = NetworkIdFromCommandLine();
+    if (network == CBaseChainParams::MAX_NETWORK_TYPES)
+        return false;
+
+    SelectParams(network);
+    return true;
+}
